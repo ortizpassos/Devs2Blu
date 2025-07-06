2d549f976fa1fb435d7e1ff6e5e6f88f3bbeeae3
ortizpassos <ortizpassos@yahoo.com.br>
2025-07-06 17:21:55 -0300

    Aulas Devs2Blu

--- Criar_Repositorio.sh
+++ Criar_Repositorio.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
-cd C:\devs2blu
+cd C:\Devs2Blu
 git init
 git add .
+git commit -m "Aulas Devs2Blu"
 git remote add origin https://github.com/ortizpassos/Devs2Blu.git
-git commit -m "Commit inicial"
-gh repo create Aula20_06 --public --source=. --push --description "Aula20_06"
+gh repo create Devs2Blu --public --source=. --push --description "Devs2Blu"
+git branch -M main
+git push -u origin main
 
 
